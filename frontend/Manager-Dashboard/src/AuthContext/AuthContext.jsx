2150a0cd7bd8110502840d169987124512ba614a
Jay Prakash <jayp7808@gmail.com>
2025-03-07 16:27:10 +0530

    first commit

--- frontend/Manager-Dashboard/src/AuthContext/AuthContext.jsx
+++ frontend/Manager-Dashboard/src/AuthContext/AuthContext.jsx
@@ -50,7 +50,7 @@ export const AuthProvider = ({ children }) => {
       axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
     } catch (error) {
       console.error("Signup failed:", error.response?.data || error.message);
-      throw new Error(error.response?.data?.message || "Signup failed");
+      error.message;
     }
   };
 
@@ -79,7 +79,7 @@ export const AuthProvider = ({ children }) => {
       axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
     } catch (error) {
       console.error("Login failed:", error.response?.data || error.message);
-      throw new Error(error.response?.data?.message || "Login failed");
+        throw  Error(error.message);
     }
   };
 
--- frontend/Manager-Dashboard/src/component/Sidebar.jsx
+++ frontend/Manager-Dashboard/src/component/Sidebar.jsx
@@ -16,7 +16,7 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
     <>
       {/* Sidebar */}
       <aside
-        className={`fixed top-0 left-0 h-screen bg-blue-900 text-white p-6 shadow-lg flex flex-col justify-between w-72 z-100 transition-transform duration-300
+        className={`fixed top-0 left-0 min-h-full bg-blue-900 text-white p-6 shadow-lg flex flex-col justify-between w-72 z-100 transition-transform duration-300
           ${isOpen ? "translate-x-0" : "-translate-x-full"} lg:translate-x-0`}
       >
         {/* Close Button (Mobile) */}
@@ -49,13 +49,7 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
         </div>
 
         {/* User Profile */}
-        <div className="flex items-center p-4 border-t border-gray-500">
-          <FaUserCircle className="text-4xl mr-2" />
-          <div>
-            <p className="font-semibold">Allen Frank</p>
-            <p className="text-sm text-gray-300">allen@gmail.com</p>
-          </div>
-        </div>
+      
       </aside>
 
       {/* Overlay when sidebar is open on mobile */}
